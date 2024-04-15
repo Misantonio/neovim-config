bd402878ffd9dcbef9151ce9e2248524f8144959
Misael Alarcón <misael.alarconh@gmail.com>
2024-04-15 08:18:46 -0600

    Init commit
    Initial Neovim config:
    * Lazy package manager
    * Catpuccin
    * Copilot
    * LSPs
    * Lua Line
    * Markdown preview
    * Neotree
    * Telescope
    * Treesitter

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,20 @@
+return {
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        local builtin = require("telescope.builtin")
+        local telescope = require("telescope")
+        telescope.setup({
+            pickers={
+                find_files = {
+                    hidden = true
+                },
+                live_grep = {
+                    hidden = true
+                }
+            }
+        })
+        vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    end
+}
